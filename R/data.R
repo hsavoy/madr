70e0d75d5dc30b67519dfffb6beca4bd39a5260b
hsavoy <heather.m.savoy@gmail.com>
2016-05-29 18:49:40 -0700

    added pumping dataset

--- R/data.R
+++ R/data.R
@@ -2,12 +2,12 @@
 #'
 #' A dataset containing a MADproject object called 'pumping'
 #'
-#' @format A MADproject object with an observed time series, two samples,
-#' and 100 realizations per sample.
+#' @format A MADproject object with an observed time series, four samples,
+#' and realizations for each sample.
 #' \describe{
 #'   \item{@observations}{a time series of drawdown, in meters}
-#'   \item{@numSamples}{the value 2, [unitless]}
-#'   \item{@realizations}{a list of length 2 with matrices
+#'   \item{@numSamples}{the value 4, [unitless]}
+#'   \item{@realizations}{a list of length 4 with matrices
 #'     containing an ensemble of 100 simulated drawdown time series,
 #'     in meters}
 #'   ...
